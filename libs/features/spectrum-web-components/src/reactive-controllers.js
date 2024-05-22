eb3526209502c3efa00ed47fa9f7b2c429557972
Gautam Bajaj <gautambajaj91@gmail.com>
2024-05-22 10:53:53 +0530

    Trial 2

--- libs/features/spectrum-web-components/src/reactive-controllers.js
+++ libs/features/spectrum-web-components/src/reactive-controllers.js
@@ -1,3 +1,3 @@
 export * from '@spectrum-web-components/reactive-controllers/src/index.js';
 export * from '@spectrum-web-components/reactive-controllers/src/ElementResolution.js';
-export * from '@spectrum-web-components/reactive-controllers/src/DependencyManger.js';
+export {DependencyManagerController} from '@spectrum-web-components/reactive-controllers/src/DependencyManger.js';
